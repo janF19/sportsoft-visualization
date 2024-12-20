1909dee203baf9115a647740bee42696865aac36
janf19 <jfaller@seznam.cz>
2024-12-20 03:42:50 +0100

    changed port

--- run.py
+++ run.py
@@ -7,7 +7,7 @@ import os
 
 # Environment configuration
 IS_PRODUCTION = os.getenv('RUNNING_IN_PRODUCTION', 'false').lower() == 'true'
-PORT = int(os.getenv('PORT', 5000))
+PORT = int(os.getenv('PORT', 10000))
 HOST = '0.0.0.0' if IS_PRODUCTION else 'localhost'
 
 if __name__ == "__main__":
